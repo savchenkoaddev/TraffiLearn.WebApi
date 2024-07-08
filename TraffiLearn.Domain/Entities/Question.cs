c64f3e2917ac81504ed08adcffc408cfc9c1bda5
savchenkoaddev <p.savchenkoad@gmail.com>
2024-07-08 13:58:46 +0300

    Add Entities

---
+++ TraffiLearn.Domain/Entities/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace TraffiLearn.Domain.Entities
+{
+    public sealed class Question
+    {
+        public string Text { get; set; }
+        public List<string> PossibleAnswears { get; set; }
+        public List<string> CorrectAnswears { get; set; }
+        public QuestionNumberDetails NumberDetails { get; set; }
+    }
+
+    public sealed record QuestionNumberDetails(int TickerNumber, int QuestionNumber);
+}
