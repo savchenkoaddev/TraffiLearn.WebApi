3ea71bf85f99fc3734dc926966cfd9e9ac8af95e
savchenkoaddev <p.savchenkoad@gmail.com>
2024-07-09 16:40:39 +0300

    Add migration

---
+++ TraffiLearn.Infrastructure/Migrations/20240709133950_CategoriesFeature.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TraffiLearn.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoriesFeature : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DrivingCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrivingCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DrivingCategoryQuestion",
+                columns: table => new
+                {
+                    DrivingCategoriesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrivingCategoryQuestion", x => new { x.DrivingCategoriesId, x.QuestionsId });
+                    table.ForeignKey(
+                        name: "FK_DrivingCategoryQuestion_DrivingCategories_DrivingCategoriesId",
+                        column: x => x.DrivingCategoriesId,
+                        principalTable: "DrivingCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DrivingCategoryQuestion_Questions_QuestionsId",
+                        column: x => x.QuestionsId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DrivingCategoryQuestion_QuestionsId",
+                table: "DrivingCategoryQuestion",
+                column: "QuestionsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DrivingCategoryQuestion");
+
+            migrationBuilder.DropTable(
+                name: "DrivingCategories");
+        }
+    }
+}
