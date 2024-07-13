a47d534e1b00ac4473cadf493d21345294e2d0ee
savchenkoaddev <p.savchenkoad@gmail.com>
2024-07-13 07:41:47 +0300

    Update Topics Queries

---
+++ TraffiLearn.Application/Topics/Queries/GetQuestionsForTopic/GetQuestionsForTopicQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TraffiLearn.Application.DTO.Questions.Response;
+
+namespace TraffiLearn.Application.Topics.Queries.GetQuestionsForTopic
+{
+    public sealed record GetQuestionsForTopicQuery(
+        Guid? TopicId) : IRequest<IEnumerable<QuestionResponse>>;
+}
---
+++ TraffiLearn.Application/Topics/Queries/GetQuestionsForTopic/GetQuestionsForTopicQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraffiLearn.Application.Topics.Queries.GetQuestionsForTopic
+{
+    internal class GetQuestionsForTopicQueryValidator
+    {
+    }
+}
