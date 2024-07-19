2b1c34f587cd1c2415c4a6f2edadf41b5e5bd47c
savchenkoaddev <p.savchenkoad@gmail.com>
2024-07-19 13:57:10 +0300

    Implement AddQuestion View

--- TraffiLearn.Application/DTO/Questions/Request/QuestionCreateRequest.cs
+++ TraffiLearn.Application/DTO/Questions/Request/QuestionCreateRequest.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using TraffiLearn.Application.DTO.Answers.Request;
+﻿using TraffiLearn.Application.DTO.Answers.Request;
 using TraffiLearn.Domain.ValueObjects;
 
 namespace TraffiLearn.Application.DTO.Questions.Request
