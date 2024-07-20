37eba1cf4ef3ee10a428f443d1349fdf0776a108
savchenkoaddev <p.savchenkoad@gmail.com>
2024-07-20 19:28:34 +0300

    Implement update functionality for questions

--- TraffiLearn.WebApp/Controllers/QuestionsController.cs
+++ TraffiLearn.WebApp/Controllers/QuestionsController.cs
@@ -5,7 +5,10 @@ using TraffiLearn.Application.DTO.Questions.Request;
 using TraffiLearn.Application.DTO.Questions.Response;
 using TraffiLearn.Application.Questions.Commands.CreateQuestion;
 using TraffiLearn.Application.Questions.Commands.DeleteQuestion;
+using TraffiLearn.Application.Questions.Commands.UpdateQuestion;
 using TraffiLearn.Application.Questions.Queries.GetAllQuestions;
+using TraffiLearn.Application.Questions.Queries.GetQuestionById;
+using TraffiLearn.Application.Questions.Queries.GetTopicsForQuestion;
 using TraffiLearn.Application.Topics.Queries.GetAll;
 using TraffiLearn.WebApp.Helpers;
 using TraffiLearn.WebApp.Options;
@@ -53,6 +56,38 @@ namespace TraffiLearn.WebApp.Controllers
             ViewBag.TotalPages = paginationHelper.TotalPages;
 
             return View(paginatedQuestions);
+        }
+
+        [HttpGet("/questions/{id:guid}/update")]
+        public async Task<IActionResult> UpdateQuestion(
+            Guid? id,
+            string? returnUrl)
+        {
+            ViewBag.QuestionId = id;
+            ViewBag.AllTopics = await _sender.Send(new GetAllSortedTopicsQuery());
+            ViewBag.QuestionTopics = await _sender.Send(new GetTopicsForQuestionQuery(id));
+            ViewBag.ReturnUrl = returnUrl;
+
+            var question = await _sender.Send(new GetQuestionByIdQuery(id));
+
+            return View(question);
+        }
+
+        [HttpPost("/questions/{id:guid}/update")]
+        public async Task<IActionResult> UpdateQuestion(
+            [FromRoute] Guid? id,
+            [FromForm] QuestionUpdateRequest? question,
+            IFormFile? image,
+            string? returnUrl)
+        {
+            await _sender.Send(new UpdateQuestionCommand(id, question, image));
+
+            if (returnUrl is not null)
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction(nameof(AllQuestions));
         }
 
         [HttpDelete("/questions/{id:guid}")]
