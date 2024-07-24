299e26d37aacfa0f5145d89bb5dd57638a4dbbe6
savchenkoaddev <p.savchenkoad@gmail.com>
2024-07-24 15:10:09 +0300

    Move WebApp Project

--- TraffiLearn.WebApp/Options/PaginationSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TraffiLearn.WebApp.Options
-{
-    public sealed class PaginationSettings
-    {
-        public const string CONFIG_KEY = "PaginationSettings";
-
-        public int? PageSize { get; set; }
-    }
-}
