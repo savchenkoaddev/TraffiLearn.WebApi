216d8759cddf4771d2314aea4ce14ddc5f81f7e5
savchenkoaddev <p.savchenkoad@gmail.com>
2024-07-29 13:53:29 +0300

    Introduce Validation Result abstractions

---
+++ TraffiLearn.Domain/Shared/IValidationResult.cs
@@ -0,0 +1,12 @@
+﻿namespace TraffiLearn.Domain.Shared
+{
+    public interface IValidationResult
+    {
+        public static readonly Error ValidationError =
+            Error.Validation(
+                code: "ValidationError",
+                description: "A validation problem occurred.");
+
+        Error[] Errors { get; }
+    }
+}
