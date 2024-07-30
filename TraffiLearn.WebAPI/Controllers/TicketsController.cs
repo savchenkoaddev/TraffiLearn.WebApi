cfe498a4be774e1eb7c896e299531b41364b006e
savchenkoaddev <p.savchenkoad@gmail.com>
2024-07-30 14:35:08 +0300

    Add Endpoints

---
+++ TraffiLearn.WebAPI/Controllers/TicketsController.cs
@@ -0,0 +1,113 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TraffiLearn.Application.Commands.Tickets.AddQuestionToTicket;
+using TraffiLearn.Application.Commands.Tickets.Create;
+using TraffiLearn.Application.Commands.Tickets.Delete;
+using TraffiLearn.Application.Commands.Tickets.RemoveQuestionFromTicket;
+using TraffiLearn.Application.Commands.Tickets.Update;
+using TraffiLearn.Application.Queries.Tickets.GetAll;
+using TraffiLearn.Application.Queries.Tickets.GetById;
+using TraffiLearn.Application.Queries.Tickets.GetQuestionsForTicket;
+using TraffiLearn.WebAPI.Extensions;
+
+namespace TraffiLearn.WebAPI.Controllers
+{
+    [Route("api/tickets")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public TicketsController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        #region Queries
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTickets()
+        {
+            var queryResult = await _sender.Send(new GetAllTicketsQuery());
+
+            return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
+        }
+
+        [HttpGet("{ticketId:guid}")]
+        public async Task<IActionResult> GetTicketById(
+            [FromRoute] Guid ticketId)
+        {
+            var queryResult = await _sender.Send(new GetTicketByIdQuery(ticketId));
+
+            return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
+        }
+
+        [HttpGet("{ticketId:guid}/questions")]
+        public async Task<IActionResult> GetQuestionsForTicket(
+            [FromRoute] Guid ticketId)
+        {
+            var queryResult = await _sender.Send(new GetQuestionsForTicketQuery(ticketId));
+
+            return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
+        }
+
+
+        #endregion
+
+        #region Commands
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(CreateTicketCommand command)
+        {
+            var commandResult = await _sender.Send(command);
+
+            return commandResult.IsSuccess ? Created() : commandResult.ToProblemDetails();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTicket(UpdateTicketCommand command)
+        {
+            var commandResult = await _sender.Send(command);
+
+            return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
+        }
+
+        [HttpPut("{ticketId:guid}/addquestion/{questionId:guid}")]
+        public async Task<IActionResult> AddQuestionToTicket(
+            [FromRoute] Guid questionId,
+            [FromRoute] Guid ticketId)
+        {
+            var commandResult = await _sender.Send(new AddQuestionToTicketCommand(
+                QuestionId: questionId,
+                TicketId: ticketId));
+
+            return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
+        }
+
+        [HttpPut("{ticketId:guid}/removequestion/{questionId:guid}")]
+        public async Task<IActionResult> RemoveQuestionFromTicket(
+            [FromRoute] Guid questionId,
+            [FromRoute] Guid ticketId)
+        {
+            var commandResult = await _sender.Send(new RemoveQuestionFromTicketCommand(
+                QuestionId: questionId,
+                TicketId: ticketId));
+
+            return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
+        }
+
+        [HttpDelete("{ticketId:guid}")]
+        public async Task<IActionResult> DeleteTicket(
+            [FromRoute] Guid ticketId)
+        {
+            var commandResult = await _sender.Send(new DeleteTicketCommand(ticketId));
+
+            return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
+        }
+
+
+        #endregion
+    }
+}
