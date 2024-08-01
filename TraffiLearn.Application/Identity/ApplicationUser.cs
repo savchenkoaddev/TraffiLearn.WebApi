392419c6fbf73bbdebbbc92ab0a8a8f045e44710
savchenkoaddev <p.savchenkoad@gmail.com>
2024-08-01 11:02:45 +0300

    Add Identity User

---
+++ TraffiLearn.Application/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TraffiLearn.Application.Identity
+{
+    public sealed class ApplicationUser : IdentityUser
+    {
+        public string Username { get; }
+    }
+}
