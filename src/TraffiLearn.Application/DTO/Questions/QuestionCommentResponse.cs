d1823822c0059e741cef872a31ddef223bd9b3ac
savchenkoaddev <p.savchenkoad@gmail.com>
2024-08-03 15:28:06 +0300

    Add DTO

---
+++ src/TraffiLearn.Application/DTO/Questions/QuestionCommentResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace TraffiLearn.Application.DTO.Questions
+{
+    public sealed record QuestionCommentResponse(
+        Guid CommentId,
+        Guid AuthorUserId,
+        string AuthorUsername,
+        bool HasReplies = false);
+}
