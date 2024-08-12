92b51e123bbe62545f4bba732c3e9e01be5217b8
savchenkoaddev <p.savchenkoad@gmail.com>
2024-08-12 15:47:13 +0300

    Implement Dislike Comment use case

---
+++ src/TraffiLearn.Application/Commands/Users/DislikeComment/DislikeCommentCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace TraffiLearn.Application.Commands.Users.DislikeComment
+{
+    internal sealed class DislikeCommentCommandValidator
+        : AbstractValidator<DislikeCommentCommand>
+    {
+        public DislikeCommentCommandValidator()
+        {
+            RuleFor(x => x.CommentId)
+                .NotEmpty();
+        }
+    }
+}
--- src/TraffiLearn.WebAPI/Controllers/CommentsController.cs
+++ src/TraffiLearn.WebAPI/Controllers/CommentsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using TraffiLearn.Application.Commands.Comments.DeleteComment;
 using TraffiLearn.Application.Commands.Comments.Reply;
 using TraffiLearn.Application.Commands.Comments.UpdateComment;
+using TraffiLearn.Application.Commands.Users.DislikeComment;
 using TraffiLearn.Application.Commands.Users.LikeComment;
 using TraffiLearn.Application.Queries.Comments.GetCommentReplies;
 using TraffiLearn.Infrastructure.Authentication;
@@ -69,6 +70,14 @@ namespace TraffiLearn.WebAPI.Controllers
         public async Task<IActionResult> LikeComment(Guid commentId)
         {
             var commandResult = await _sender.Send(new LikeCommentCommand(commentId));
+
+            return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
+        }
+
+        [HttpPut("{commentId:guid}/dislike")]
+        public async Task<IActionResult> DislikeComment(Guid commentId)
+        {
+            var commandResult = await _sender.Send(new DislikeCommentCommand(commentId));
 
             return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
         }
