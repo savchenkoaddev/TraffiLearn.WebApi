37069740474a5d2d80f437babd73c6ae774a05a2
savchenkoaddev <p.savchenkoad@gmail.com>
2024-08-15 15:21:42 +0300

    Implement Exception tests

---
+++ tests/TraffiLearn.ArchitectureTests/ExceptionsTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace TraffiLearn.ArchitectureTests
+{
+    public sealed class ExceptionsTests : BaseTest
+    {
+        [Fact]
+        public void Exceptions_ShouldHaveProperNaming()
+        {
+            var result = Types.InAssemblies(
+                [
+                    DomainAssembly, 
+                    ApplicationAssembly, 
+                    InfrastructureAssembly, 
+                    WebApiAssembly
+                ])
+                .That()
+                .Inherit(typeof(Exception))
+                .Should()
+                .HaveNameEndingWith("Exception")
+                .GetResult();
+
+            if (!result.IsSuccessful)
+            {
+                var invalidTypes = result.FailingTypes
+                    .Select(t => t.FullName)
+                    .ToList();
+
+                invalidTypes.Should().BeEmpty(
+                    "The following exception types should end with 'Exception': {0}",
+                    string.Join(", ", invalidTypes));
+            }
+        }
+    }
+}
