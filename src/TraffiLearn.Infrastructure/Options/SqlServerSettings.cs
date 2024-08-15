4d95d44ef6ed3efe17b86e915f1ad34a89337b1a
savchenkoaddev <p.savchenkoad@gmail.com>
2024-08-15 16:33:06 +0300

    Add options constraints

--- src/TraffiLearn.Infrastructure/Options/SqlServerSettings.cs
+++ src/TraffiLearn.Infrastructure/Options/SqlServerSettings.cs
@@ -1,9 +1,12 @@
-﻿namespace TraffiLearn.Infrastructure.Options
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraffiLearn.Infrastructure.Options
 {
     public sealed class SqlServerSettings
     {
         public const string SectionName = nameof(SqlServerSettings);
 
+        [Required]
         public string? ConnectionString { get; set; }
     }
 }
