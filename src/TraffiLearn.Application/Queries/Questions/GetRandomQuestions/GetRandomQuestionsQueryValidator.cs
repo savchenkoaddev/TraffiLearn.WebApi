19c3e00d8397c7153b65d43b8cfc60e6a2e4c178
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-08-15 18:13:34 +0200

    Implement Get Random Questions use case

---
+++ src/TraffiLearn.Application/Queries/Questions/GetRandomQuestions/GetRandomQuestionsQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace TraffiLearn.Application.Queries.Questions.GetRandomQuestions
+{
+    internal sealed class GetRandomQuestionsQueryValidator : AbstractValidator<GetRandomQuestionsQuery>
+    {
+        public GetRandomQuestionsQueryValidator()
+        {
+            RuleFor(x => x.Amount)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(1);
+        }
+    }
+}
