c6cc206daba656d2194b0b241f570499f75bda7f
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-09-10 20:40:28 +0200

    Add IAIService

---
+++ src/TraffiLearn.Infrastructure/Exceptions/ApiBadResponseException.cs
@@ -0,0 +1,11 @@
+﻿namespace TraffiLearn.Infrastructure.Exceptions
+{
+    public sealed class ApiBadResponseException : Exception
+    {
+        public ApiBadResponseException() : base("Api returned bad response.")
+        { }
+
+        public ApiBadResponseException(string apiName) : base($"{apiName} returned bad response.")
+        { }
+    }
+}
