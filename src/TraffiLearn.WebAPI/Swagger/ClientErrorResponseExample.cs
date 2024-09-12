1dadaf66f96602568d7225cd2b85d526b2ec42ea
savchenkoaddev <p.savchenkoad@gmail.com>
2024-09-12 12:14:33 +0300

    Implement swagger docs for auth

---
+++ src/TraffiLearn.WebAPI/Swagger/ClientErrorResponseExample.cs
@@ -0,0 +1,16 @@
+﻿namespace TraffiLearn.WebAPI.Swagger
+{
+    internal sealed class ClientErrorResponseExample
+    {
+        private ClientErrorResponseExample()
+        { }
+
+        public string? Type { get; set; }
+
+        public string? Title { get; set; }
+
+        public int Status { get; set; }
+
+        public ErrorResponseExample[]? Errors { get; set; }
+    }
+}
---
+++ src/TraffiLearn.WebAPI/Swagger/ServerErrorResponseExample.cs
@@ -0,0 +1,14 @@
+﻿namespace TraffiLearn.WebAPI.Swagger
+{
+    internal sealed class ServerErrorResponseExample
+    {
+        private ServerErrorResponseExample()
+        { }
+
+        public string? Type => "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+
+        public string? Title => "Internal Server Error";
+
+        public int Status => 500;
+    }
+}
