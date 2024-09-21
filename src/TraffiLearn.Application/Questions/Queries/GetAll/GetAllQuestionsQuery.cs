3418cb90021a9643e1f3500ae1b34bb5be5c6e7d
savchenkoaddev <p.savchenkoad@gmail.com>
2024-09-21 11:04:36 +0300

    Change GetAllQuestions query

--- src/TraffiLearn.Application/Questions/Queries/GetAll/GetAllQuestionsQuery.cs
+++ src/TraffiLearn.Application/Questions/Queries/GetAll/GetAllQuestionsQuery.cs
@@ -4,5 +4,7 @@ using TraffiLearn.Domain.Shared;
 
 namespace TraffiLearn.Application.Questions.Queries.GetAll
 {
-    public sealed record GetAllQuestionsQuery : IRequest<Result<IEnumerable<QuestionResponse>>>;
+    public sealed record GetAllQuestionsQuery(
+        int Page = 1,
+        int PageSize = 10) : IRequest<Result<IEnumerable<QuestionResponse>>>;
 }
---
+++ src/TraffiLearn.Application/Questions/Queries/GetAll/GetAllQuestionsQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace TraffiLearn.Application.Questions.Queries.GetAll
+{
+    internal sealed class GetAllQuestionsQueryValidator 
+        : AbstractValidator<GetAllQuestionsQuery>
+    {
+        public GetAllQuestionsQueryValidator()
+        {
+            RuleFor(x => x.Page)
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(x => x.PageSize)
+                .InclusiveBetween(1, 30);
+        }
+    }
+}
