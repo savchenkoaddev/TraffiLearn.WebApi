48ca4df0aaff88f028b924f464508af0d5312d6e
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-09-25 12:33:17 +0200

    Add documentation to topics endpoints

--- src/TraffiLearn.WebAPI/Controllers/TopicsController.cs
+++ src/TraffiLearn.WebAPI/Controllers/TopicsController.cs
@@ -1,16 +1,20 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using TraffiLearn.Application.Questions.DTO;
 using TraffiLearn.Application.Topics.Commands.AddQuestionToTopic;
 using TraffiLearn.Application.Topics.Commands.Create;
 using TraffiLearn.Application.Topics.Commands.Delete;
 using TraffiLearn.Application.Topics.Commands.RemoveQuestionFromTopic;
 using TraffiLearn.Application.Topics.Commands.Update;
+using TraffiLearn.Application.Topics.DTO;
 using TraffiLearn.Application.Topics.Queries.GetAllSortedByNumber;
 using TraffiLearn.Application.Topics.Queries.GetById;
 using TraffiLearn.Application.Topics.Queries.GetRandomTopicWithQuestions;
 using TraffiLearn.Application.Topics.Queries.GetTopicQuestions;
 using TraffiLearn.Infrastructure.Authentication;
 using TraffiLearn.WebAPI.Extensions;
+using TraffiLearn.WebAPI.Swagger;
 
 namespace TraffiLearn.WebAPI.Controllers
 {
@@ -29,7 +33,20 @@ namespace TraffiLearn.WebAPI.Controllers
         #region Queries
 
 
+        /// <summary>
+        /// Gets all topics sorted by number from the storage.
+        /// </summary>
+        /// <remarks>
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token.
+        /// </remarks>
+        /// <response code="200">Successfully retrieved all topics. Returns a list of topics.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HttpGet]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(IEnumerable<TopicResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAllSortedTopicsByNumber()
         {
             var queryResult = await _sender.Send(new GetAllSortedTopicsByNumberQuery());
@@ -37,7 +54,19 @@ namespace TraffiLearn.WebAPI.Controllers
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Gets a random topic with questions included.
+        /// </summary>
+        /// <remarks>
+        /// The user must be authenticated using a JWT token.
+        /// </remarks>
+        /// <response code="200">Successfully retrieved topic with questions. Returns a topic.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HttpGet("random/with-questions")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(TopicWithQuestionsResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetRandomTopicWithQuestions()
         {
             var queryResult = await _sender.Send(new GetRandomTopicWithQuestionsQuery());
@@ -45,7 +74,26 @@ namespace TraffiLearn.WebAPI.Controllers
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Gets a topic with a specific ID.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include an ID of a topic to get.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `TopicId` : Must be a valid GUID representing ID of a topic.<br /><br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token.<br /><br />
+        /// </remarks>
+        /// <param name="topicId">**The ID of a topic to be retrieved**</param>
+        /// <response code="200">Successfully retrieved the topic with the provided ID. Returns the found topic.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="404">***Not found.*** No topic exists with the provided ID.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HttpGet("{topicId:guid}")]
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(TopicResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetTopicById(
             [FromRoute] Guid topicId)
         {
@@ -54,7 +102,26 @@ namespace TraffiLearn.WebAPI.Controllers
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Gets all questions associated with a topic by a topic ID.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include an ID of a topic.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `TopicId` : Must be a valid GUID representing ID of a topic.<br /><br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token.<br /><br />
+        /// </remarks>
+        /// <param name="topicId">**The ID of a topic used to find related questions.**</param>
+        /// <response code="200">Successfully retrieved questions associated with the topic with the provided ID. Returns a list of questions.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="404">***Not found.*** No topic exists with the provided ID.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HttpGet("{topicId:guid}/questions")]
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(IEnumerable<QuestionResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetTopicQuestions(
             [FromRoute] Guid topicId)
         {
@@ -69,8 +136,29 @@ namespace TraffiLearn.WebAPI.Controllers
         #region Commands
 
 
+        /// <summary>
+        /// Creates a new topic.
+        /// </summary>
+        /// <remarks>
+        /// ***Parameters:***<br /><br />
+        /// `TopicNumber` : Number of the topic. Must be greater than 0.<br /><br />
+        /// `Title` : Title of the topic. Must be less than 300 characters long.<br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="command">**The create topic command.**</param>
+        /// <response code="201">Successfully created a new topic. Returns ID of a newly created topic</response>
+        /// <response code="400">***Bad request.*** The provided data is invalid or missing.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.ModifyData)]
         [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CreateTopic(CreateTopicCommand command)
         {
             var commandResult = await _sender.Send(command);
@@ -86,8 +174,31 @@ namespace TraffiLearn.WebAPI.Controllers
             return commandResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Updates an existing topic.
+        /// </summary>
+        /// <remarks>
+        /// ***Parameters:***<br /><br />
+        /// `TopicId` : ID of the topic to be updated. Must be a valid GUID.<br /><br />
+        /// `TopicNumber` : Number of the topic. Must be greater than 0.<br /><br />
+        /// `Title` : Title of the topic. Must be less than 300 characters long.<br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="command">**The update topic command.**</param>
+        /// <response code="204">Successfully updated an existing topic.</response>
+        /// <response code="400">***Bad request.*** The provided data is invalid or missing.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** Topic with the id is not found.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.ModifyData)]
         [HttpPut]
+        [Consumes(MediaTypeNames.Application.Json)]             
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> UpdateTopic(UpdateTopicCommand command)
         {
             var commandResult = await _sender.Send(command);
@@ -95,8 +206,31 @@ namespace TraffiLearn.WebAPI.Controllers
             return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Adds a question to a topic.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include the ID of the question and topic.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `QuestionId` : Must be a valid GUID representing ID of the question.<br /><br />
+        /// `TopicId` : Must be a valid GUID representing ID of the topic.<br /><br />
+        /// ***Authentication Required:***<br /><br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="questionId">**The ID of the question to be added to topic.**</param>
+        /// <param name="topicId">**The ID of the topic to which the question will be added.**</param>
+        /// <response code="204">Successfully added question to topic.</response>
+        /// <response code="400">***Bad request.*** The provided data is invalid or missing.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** Question or topic with the provided id is not found.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.ModifyData)]
         [HttpPut("{topicId:guid}/add-question/{questionId:guid}")]
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> AddQuestionToTopic(
             [FromRoute] Guid questionId,
             [FromRoute] Guid topicId)
@@ -108,8 +242,31 @@ namespace TraffiLearn.WebAPI.Controllers
             return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Removes a question from a topic.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include the ID of the question and topic.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `QuestionId` : Must be a valid GUID representing ID of the question.<br /><br />
+        /// `TopicId` : Must be a valid GUID representing ID of the topic.<br /><br />
+        /// ***Authentication Required:***<br /><br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="questionId">**The ID of the question to be removed from the topic.**</param>
+        /// <param name="topicId">**The ID of the topic from which the question will be removed.**</param>
+        /// <response code="204">Successfully removed the question from the topic.</response>
+        /// <response code="400">***Bad request.*** The provided data is invalid or missing.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** Question or topic with the provided id is not found.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.ModifyData)]
         [HttpPut("{topicId:guid}/remove-question/{questionId:guid}")]
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> RemoveQuestionFromTopic(
             [FromRoute] Guid questionId,
             [FromRoute] Guid topicId)
@@ -121,8 +278,27 @@ namespace TraffiLearn.WebAPI.Controllers
             return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Deletes a topic using its ID.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include the ID of the topic.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `TopicId` : Must be a valid GUID representing ID of the topic.<br /><br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="topicId">**The ID of the topic to be deleted.**</param>
+        /// <response code="204">Successfully deleted the topic.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** Question with the provided id is not found.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.ModifyData)]
         [HttpDelete("{topicId:guid}")]
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteTopic(
             [FromRoute] Guid topicId)
         {
