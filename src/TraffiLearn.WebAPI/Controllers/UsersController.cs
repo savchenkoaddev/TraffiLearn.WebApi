3a602b9603e7067ac0bed46832eb19c1622d6317
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-09-28 14:27:12 +0200

    TRAF-72: Users swagger documentation (#61)
    https://psavchenkoad.atlassian.net/browse/TRAF-72
    * Add documentation for users endpoints
    * Minor fixes
    * Resolve changes

--- src/TraffiLearn.WebAPI/Controllers/UsersController.cs
+++ src/TraffiLearn.WebAPI/Controllers/UsersController.cs
@@ -1,6 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using TraffiLearn.Application.Comments.DTO;
+using TraffiLearn.Application.Questions.DTO;
 using TraffiLearn.Application.Users.Commands.DowngradeAccount;
+using TraffiLearn.Application.Users.DTO;
 using TraffiLearn.Application.Users.Queries.GetAllAdmins;
 using TraffiLearn.Application.Users.Queries.GetAllUsers;
 using TraffiLearn.Application.Users.Queries.GetLoggedInUserComments;
@@ -9,6 +13,7 @@ using TraffiLearn.Application.Users.Queries.GetUserDislikedQuestions;
 using TraffiLearn.Application.Users.Queries.GetUserLikedQuestions;
 using TraffiLearn.Infrastructure.Authentication;
 using TraffiLearn.WebAPI.Extensions;
+using TraffiLearn.WebAPI.Swagger;
 
 namespace TraffiLearn.WebAPI.Controllers
 {
@@ -25,8 +30,23 @@ namespace TraffiLearn.WebAPI.Controllers
 
         #region Queries
 
+
+        /// <summary>
+        /// Gets all users from the storage.
+        /// </summary>
+        /// <remarks>
+        ///  **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <response code="200">Successfully retrieved all users. Returns a list of users.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.AccessSpecificUserData)]
         [HttpGet]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(IEnumerable<UserResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAllUsers()
         {
             var queryResult = await _sender.Send(new GetAllUsersQuery());
@@ -34,8 +54,22 @@ namespace TraffiLearn.WebAPI.Controllers
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Gets all admins from the storage.
+        /// </summary>
+        /// <remarks>
+        ///  **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <response code="200">Successfully retrieved all admins. Returns a list of admins.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.AccessSpecificAdminData)]
         [HttpGet("admins")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(IEnumerable<UserResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAllAdmins()
         {
             var queryResult = await _sender.Send(new GetAllAdminsQuery());
@@ -43,35 +77,111 @@ namespace TraffiLearn.WebAPI.Controllers
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Gets all comments of a user with a specific ID.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include an ID of a user.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `UserId` : Must be a valid GUID representing ID of a user.<br /><br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="userId">**The ID of a user, whose comments are being retrieved.**</param>
+        /// <response code="200">Successfully retrieved user comments with the provided user ID. Returns user comments.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** No user exists with the provided ID.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.AccessSpecificUserData)]
         [HttpGet("{userId:guid}/comments")]
-        public async Task<IActionResult> GetUserComments(Guid userId)
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(IEnumerable<CommentResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetUserComments(
+            [FromRoute] Guid userId)
         {
             var queryResult = await _sender.Send(new GetUserCommentsQuery(userId));
 
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Gets all liked questions of a user with a specific ID.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include an ID of a user.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `UserId` : Must be a valid GUID representing ID of a user.<br /><br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="userId">**The ID of a user, whose liked questions are being retrieved.**</param>
+        /// <response code="200">Successfully retrieved user liked questions with the provided user ID. Returns user liked questions.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** No user exists with the provided ID.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.AccessSpecificUserData)]
         [HttpGet("{userId:guid}/liked-questions")]
-        public async Task<IActionResult> GetUserLikedQuestions(Guid userId)
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(IEnumerable<QuestionResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetUserLikedQuestions(
+            [FromRoute] Guid userId)
         {
             var queryResult = await _sender.Send(new GetUserLikedQuestionsQuery(userId));
 
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Gets all disliked questions of a user with a specific ID.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include an ID of a user.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `UserId` : Must be a valid GUID representing ID of a user.<br /><br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="userId">**The ID of a user, whose disliked questions are being retrieved.**</param>
+        /// <response code="200">Successfully retrieved user disliked questions with the provided user ID. Returns user disliked questions.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** No user exists with the provided ID.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.AccessSpecificUserData)]
         [HttpGet("{userId:guid}/disliked-questions")]
-        public async Task<IActionResult> GetUserDislikedQuestions(Guid userId)
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(IEnumerable<QuestionResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetUserDislikedQuestions(
+            [FromRoute] Guid userId)
         {
             var queryResult = await _sender.Send(new GetUserDislikedQuestionsQuery(userId));
 
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
 
+        /// <summary>
+        /// Gets all comments of the currently logged in user.
+        /// </summary>
+        /// <remarks>
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token.
+        /// </remarks>
+        /// <response code="200">Successfully retrieved logged in user comments. Returns logged in user comments.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.AccessData)]
         [HttpGet("current/comments")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(IEnumerable<CommentResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetLoggedInUserComments()
         {
             var queryResult = await _sender.Send(new GetLoggedInUserCommentsQuery());
@@ -85,9 +195,31 @@ namespace TraffiLearn.WebAPI.Controllers
         #region Commands
 
 
+        /// <summary>
+        /// Downgrades the account of a user with a specific ID.
+        /// </summary>
+        /// <remarks>
+        /// **If account getting downgraded has role `Admin`**, user will be downgraded to the role `RegularUser`.<br /><br />
+        /// **`RegularUser` and `Owner` roles cannot be downgraded.**<br /><br />
+        /// **The request must include an ID of a user.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `UserId` : Must be a valid GUID representing ID of a user.<br /><br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="userId">**The ID of user, who is being downgraded.**</param>
+        /// <response code="204">Successfully downgraded the user with the provided ID.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** No user exists with the provided ID.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
         [HasPermission(Permission.DowngradeAccounts)]
         [HttpPut("{userId:guid}/downgrade")]
-        public async Task<IActionResult> DowngradeAccount(Guid userId)
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DowngradeAccount(
+            [FromRoute] Guid userId)
         {
             var commandResult = await _sender.Send(new DowngradeAccountCommand(userId));
 
