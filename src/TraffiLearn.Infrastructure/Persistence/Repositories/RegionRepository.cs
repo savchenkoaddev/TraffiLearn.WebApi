0b176292d04c181331e5b00b70965b906276ce76
savchenkoaddev <p.savchenkoad@gmail.com>
2024-10-02 09:11:54 +0300

    Implement Region Repository

---
+++ src/TraffiLearn.Infrastructure/Persistence/Repositories/RegionRepository.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraffiLearn.Domain.Aggregates.Regions;
+using TraffiLearn.Domain.Aggregates.Regions.ValueObjects;
+
+namespace TraffiLearn.Infrastructure.Persistence.Repositories
+{
+    internal sealed class RegionRepository : IRegionRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RegionRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task DeleteAsync(Region region)
+        {
+            EnsurePassedRegionIsNotNull(region);
+
+            _dbContext.Regions.Remove(region);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task<bool> ExistsAsync(
+            RegionId regionId,
+            CancellationToken cancellationToken = default)
+        {
+            return await GetByIdAsync(
+                regionId,
+                cancellationToken) is not null;
+        }
+
+        public async Task<IEnumerable<Region>> GetAllRegionsAsync(
+            CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Regions.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Region?> GetByIdAsync(
+            RegionId regionId,
+            CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Regions.FindAsync(
+                keyValues: [regionId],
+                cancellationToken);
+        }
+
+        public async Task InsertAsync(
+            Region region,
+            CancellationToken cancellationToken = default)
+        {
+            EnsurePassedRegionIsNotNull(region);
+
+            await _dbContext.Regions.AddAsync(
+                region,
+                cancellationToken);
+        }
+
+        public Task UpdateAsync(Region region)
+        {
+            EnsurePassedRegionIsNotNull(region);
+
+            _dbContext.Regions.Update(region);
+
+            return Task.CompletedTask;
+        }
+
+        private static void EnsurePassedRegionIsNotNull(Region region)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(region), "Region can't be null");
+        }
+    }
+}
