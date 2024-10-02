20920fb15fe2d2813fd0c46625a5128a6b67e9fd
savchenkoaddev <p.savchenkoad@gmail.com>
2024-10-02 12:18:25 +0300

    Implement CreateRegion Handler

---
+++ src/TraffiLearn.Application/Regions/Commands/Create/CreateRegionCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TraffiLearn.Domain.Aggregates.Regions.ValueObjects;
+
+namespace TraffiLearn.Application.Regions.Commands.Create
+{
+    internal sealed class CreateRegionCommandValidator
+        : AbstractValidator<CreateRegionCommand>
+    {
+        public CreateRegionCommandValidator()
+        {
+            RuleFor(x => x.RegionName)
+                .NotEmpty()
+                .MaximumLength(RegionName.MaxLength);
+        }
+    }
+}
