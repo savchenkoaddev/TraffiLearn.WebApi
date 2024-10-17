acd9aebc83bd6d710839948484a95ed3b29347c5
savchenkoaddev <p.savchenkoad@gmail.com>
2024-10-17 09:02:36 +0300

    Add migration

---
+++ src/TraffiLearn.Infrastructure/Migrations/20241017060219_TopicImages.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TraffiLearn.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TopicImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUri",
+                table: "Topics",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUri",
+                table: "Topics");
+        }
+    }
+}
