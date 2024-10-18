6caacc8af8653e9a91a4acdc999a98d72a8556f9
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-10-18 19:00:43 +0200

    Implement DeleteDirectory Endpoint

---
+++ src/TraffiLearn.Application/Directories/Commands/Delete/DeleteDirectoryCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace TraffiLearn.Application.Directories.Commands.Delete
+{
+    internal sealed class DeleteDirectoryCommandValidator
+        : AbstractValidator<DeleteDirectoryCommand>
+    {
+        public DeleteDirectoryCommandValidator()
+        {
+            RuleFor(x => x.DirectoryId)
+                .NotEmpty();
+        }
+    }
+}
