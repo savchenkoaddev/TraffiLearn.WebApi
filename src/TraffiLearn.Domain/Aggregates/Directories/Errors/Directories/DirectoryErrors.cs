8e4f46f25d1dc492ed60ddc9ba7bd9b24f4af032
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-10-18 19:43:23 +0200

    Implement UpdateDirectory Endpoint

--- src/TraffiLearn.Domain/Aggregates/Directories/Errors/Directories/DirectoryErrors.cs
+++ src/TraffiLearn.Domain/Aggregates/Directories/Errors/Directories/DirectoryErrors.cs
@@ -8,5 +8,15 @@ namespace TraffiLearn.Domain.Aggregates.Directories.Errors.Directories
             Error.Validation(
                 code: "Directory.EmptySections",
                 description: "Sections cannot be empty.");
+
+        public static Error TooManySections(int allowedSectionCount) =>
+            Error.Validation(
+                code: "Directory.TooManySections",
+                description: $"Sections count cannot exceed {allowedSectionCount}.");
+
+        public static readonly Error NotFound =
+            Error.NotFound(
+                code: "Directory.NotFound",
+                description: "Directory has not been found.");
     }
 }
--- src/TraffiLearn.WebAPI/Controllers/DirectoriesController.cs
+++ src/TraffiLearn.WebAPI/Controllers/DirectoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
+using TraffiLearn.Application.Directories.Commands.Update;
 using TraffiLearn.Application.Directories.DTO;
 using TraffiLearn.Application.Directories.Queries.GetAll;
 using TraffiLearn.Infrastructure.Authentication;
@@ -44,6 +45,44 @@ namespace TraffiLearn.WebAPI.Controllers
 
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
+
+
+        #endregion
+
+        #region Commands
+
+
+        /// <summary>
+        /// Updates an existing directory.
+        /// </summary>
+        /// <remarks>
+        /// ***Parameters:***<br /><br />
+        /// `Name`: Name of the directory to create. Must not be empty or whitespace. Maximum length: 200. <br /><br />
+        /// `Sections`: List of sections to create in the directory. Must not be empty. Must contain at least one section. Maximum count: 100. <br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token. Only users with the `Owner` or `Admin` role can perform this action.<br /><br />
+        /// </remarks>
+        /// <param name="command">The update directory command.</param>
+        /// <response code="204">Successfully updated an existing directory.</response>
+        /// <response code="400">***Bad request.*** The provided data is invalid or missing.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="403">***Forbidden***. The user is not authorized to perform this action.</response>
+        /// <response code="404">***Not found.*** Directory with the ID is not found.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
+        [HasPermission(Permission.ModifyData)]
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateDirectory(
+            [FromBody] UpdateDirectoryCommand command)
+        {
+            var commandResult = await _sender.Send(command);
+
+            return commandResult.IsSuccess ? Ok() : commandResult.ToProblemDetails();
+        }
+
 
         #endregion
     }
