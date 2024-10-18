d3457bbf02c13f164c9d68533bf4f1f0fd8b74dd
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-10-18 20:10:51 +0200

    TRAF-176: Implement GetDirectoryById Endpoint (#83)
    https://psavchenkoad.atlassian.net/browse/TRAF-176
    * Implement GetDirectoryById Endpoint
    * Aplly requested changes

---
+++ src/TraffiLearn.Application/Directories/Queries/GetById/GetDirectoryByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace TraffiLearn.Application.Directories.Queries.GetById
+{
+    internal sealed class GetDirectoryByIdQueryValidator
+        : AbstractValidator<GetDirectoryByIdQuery>
+    {
+        public GetDirectoryByIdQueryValidator()
+        {
+            RuleFor(x => x.DirectoryId)
+                .NotEmpty();
+        }
+    }
+}
--- src/TraffiLearn.WebAPI/Controllers/DirectoriesController.cs
+++ src/TraffiLearn.WebAPI/Controllers/DirectoriesController.cs
@@ -1,9 +1,10 @@
-﻿using MediatR;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 using TraffiLearn.Application.Directories.Commands.Delete;
 using TraffiLearn.Application.Directories.DTO;
 using TraffiLearn.Application.Directories.Queries.GetAll;
+using TraffiLearn.Application.Directories.Queries.GetById;
 using TraffiLearn.Infrastructure.Authentication;
 using TraffiLearn.WebAPI.Extensions;
 using TraffiLearn.WebAPI.Swagger;
@@ -42,6 +43,34 @@ namespace TraffiLearn.WebAPI.Controllers
         public async Task<IActionResult> GetAllDirectories()
         {
             var queryResult = await _sender.Send(new GetAllDirectoriesQuery());
+
+            return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
+        }
+
+        /// <summary>
+        /// Gets a directory with a specific ID.
+        /// </summary>
+        /// <remarks>
+        /// **The request must include an ID of a directory to get.**<br /><br /><br />
+        /// ***Route parameters:***<br /><br />
+        /// `DirectoryId` : Must be a valid GUID representing ID of a directory.<br /><br /><br />
+        /// **Authentication Required:**<br />
+        /// The user must be authenticated using a JWT token.<br /><br />
+        /// </remarks>
+        /// <param name="directoryId">**The ID of a directory to be retrieved**</param>
+        /// <response code="200">Successfully retrieved the directory with the provided ID. Returns the found directory.</response>
+        /// <response code="401">***Unauthorized.*** The user is not authenticated.</response>
+        /// <response code="404">***Not found.*** No directory exists with the provided ID.</response>
+        /// <response code="500">***Internal Server Error.*** An unexpected error occurred during the process.</response>
+        [HttpGet("{directoryId:guid}")]
+        [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType(typeof(DirectoryResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ClientErrorResponseExample), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetDirectoryById(
+            [FromRoute] Guid directoryId)
+        {
+            var queryResult = await _sender.Send(new GetDirectoryByIdQuery(directoryId));
 
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
