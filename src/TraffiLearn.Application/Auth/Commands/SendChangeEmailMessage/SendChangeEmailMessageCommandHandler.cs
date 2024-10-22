d31a850c1babc34320fc39543f5674ac9faf1b53
savchenkoaddev <p.savchenkoad@gmail.com>
2024-10-22 19:04:55 +0300

    Implement the Send Change Email Message Handler

--- src/TraffiLearn.Application/Auth/Commands/SendChangeEmailMessage/SendChangeEmailMessageCommandHandler.cs
+++ src/TraffiLearn.Application/Auth/Commands/SendChangeEmailMessage/SendChangeEmailMessageCommandHandler.cs
@@ -1,4 +1,10 @@
 ﻿using MediatR;
+using TraffiLearn.Application.Abstractions.Emails;
+using TraffiLearn.Application.Abstractions.Identity;
+using TraffiLearn.Application.Exceptions;
+using TraffiLearn.Application.Users.Identity;
+using TraffiLearn.Domain.Aggregates.Users;
+using TraffiLearn.Domain.Aggregates.Users.ValueObjects;
 using TraffiLearn.Domain.Shared;
 
 namespace TraffiLearn.Application.Auth.Commands.SendChangeEmailMessage
@@ -6,11 +12,53 @@ namespace TraffiLearn.Application.Auth.Commands.SendChangeEmailMessage
     internal sealed class SendChangeEmailMessageCommandHandler
         : IRequestHandler<SendChangeEmailMessageCommand, Result>
     {
-        public Task<Result> Handle(
-            SendChangeEmailMessageCommand request, 
+        private readonly IUserContextService<Guid> _userContextService;
+        private readonly IIdentityService<ApplicationUser> _identityService;
+        private readonly IUserRepository _userRepository;
+        private readonly IEmailService _emailService;
+
+        public SendChangeEmailMessageCommandHandler(
+            IUserContextService<Guid> userContextService,
+            IIdentityService<ApplicationUser> identityService,
+            IUserRepository userRepository,
+            IEmailService emailService)
+        {
+            _userContextService = userContextService;
+            _identityService = identityService;
+            _userRepository = userRepository;
+            _emailService = emailService;
+        }
+
+        public async Task<Result> Handle(
+            SendChangeEmailMessageCommand request,
             CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var callerUserId = _userContextService.GetAuthenticatedUserId();
+
+            var userId = new UserId(callerUserId);
+
+            var user = await _userRepository.GetByIdAsync(
+                userId, cancellationToken);
+
+            if (user is null)
+            {
+                throw new AuthorizationFailureException();
+            }
+
+            var identityUser = await _identityService.GetByEmailAsync(
+                user.Email);
+
+            if (identityUser is null)
+            {
+                throw new DataInconsistencyException();
+            }
+
+            await _emailService.SendChangeEmailMessageAsync(
+                newEmail: request.NewEmail,
+                userId: callerUserId.ToString(),
+                identityUser: identityUser);
+
+            return Result.Success();
         }
     }
 }
