6fbd18849eafd443e1044e6a93a62887dbbe842f
savchenkoaddev <p.savchenkoad@gmail.com>
2024-10-22 17:17:17 +0300

    Implement the endpoint

--- src/TraffiLearn.Application/Auth/Commands/SignInWithGoogle/SignInWithGoogleCommandValidator.cs
+++ src/TraffiLearn.Application/Auth/Commands/SignInWithGoogle/SignInWithGoogleCommandValidator.cs
@@ -1,6 +1,14 @@
-﻿namespace TraffiLearn.Application.Auth.Commands.SignInWithGoogle
+﻿using FluentValidation;
+
+namespace TraffiLearn.Application.Auth.Commands.SignInWithGoogle
 {
     internal sealed class SignInWithGoogleCommandValidator
+        : AbstractValidator<SignInWithGoogleCommand>
     {
+        public SignInWithGoogleCommandValidator()
+        {
+            RuleFor(x => x.GoogleIdToken)
+                .NotEmpty();
+        }
     }
 }
