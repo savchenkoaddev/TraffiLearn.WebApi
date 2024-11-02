5ac5dea3d04941d07d847ca309479f98a54e2873
savchenkoaddev <p.savchenkoad@gmail.com>
2024-11-02 06:46:57 +0200

    Implement the handler

--- src/TraffiLearn.Domain/SubscriptionPlans/SubscriptionPlanErrors.cs
+++ src/TraffiLearn.Domain/SubscriptionPlans/SubscriptionPlanErrors.cs
@@ -9,6 +9,11 @@ namespace TraffiLearn.Domain.SubscriptionPlans
                 code: "SubscriptionPlan.EmptyFeatures",
                 description: "Subscription plan should have at least one feature.");
 
+        public static readonly Error NotFound =
+            Error.Validation(
+                code: "SubscriptionPlan.NotFound",
+                description: "A subscription plan with the provided id has not been found.");
+
         public static Error TooMuchFeatures(int maxFeaturesCount) =>
             Error.Validation(
                 code: "SubscriptionPlan.TooMuchFeatures",
