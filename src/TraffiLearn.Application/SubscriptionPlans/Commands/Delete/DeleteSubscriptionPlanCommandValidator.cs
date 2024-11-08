a6ff92255349df149847f44ee2c28ba691e0d1f2
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-08 14:49:27 +0100

    Implement DeleteSubscriptionPlan use case

---
+++ src/TraffiLearn.Application/SubscriptionPlans/Commands/Delete/DeleteSubscriptionPlanCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace TraffiLearn.Application.SubscriptionPlans.Commands.Delete
+{
+    internal sealed class DeleteSubscriptionPlanCommandValidator
+        : AbstractValidator<DeleteSubscriptionPlanCommand>
+    {
+        public DeleteSubscriptionPlanCommandValidator()
+        {
+            RuleFor(x => x.SubscriptionPlanId)
+                .NotEmpty();
+        }
+    }
+}
