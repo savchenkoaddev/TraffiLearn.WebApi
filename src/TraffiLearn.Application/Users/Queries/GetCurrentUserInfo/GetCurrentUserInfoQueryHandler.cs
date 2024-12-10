ff0e07f6189e5bd8f7fc8d16fa2a1c78681a15a6
savchenkoaddev <p.savchenkoad@gmail.com>
2024-12-10 11:23:04 +0200

    Implement Query Handler

---
+++ src/TraffiLearn.Application/Users/Queries/GetCurrentUserInfo/GetCurrentUserInfoQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using TraffiLearn.Application.Abstractions.Data;
+using TraffiLearn.Application.Abstractions.Identity;
+using TraffiLearn.Application.Users.DTO;
+using TraffiLearn.Domain.Users;
+using TraffiLearn.SharedKernel.Shared;
+
+namespace TraffiLearn.Application.Users.Queries.GetCurrentUserInfo
+{
+    internal sealed class GetCurrentUserInfoQueryHandler
+        : IRequestHandler<GetCurrentUserInfoQuery, Result<CurrentUserResponse>>
+    {
+        private readonly IAuthenticatedUserService _authenticatedUserService;
+        private readonly Mapper<User, CurrentUserResponse> _userMapper;
+
+        public GetCurrentUserInfoQueryHandler(
+            IAuthenticatedUserService authenticatedUserService,
+            Mapper<User, CurrentUserResponse> userMapper)
+        {
+            _authenticatedUserService = authenticatedUserService;
+            _userMapper = userMapper;
+        }
+
+        public async Task<Result<CurrentUserResponse>> Handle(
+            GetCurrentUserInfoQuery request,
+            CancellationToken cancellationToken)
+        {
+            var user = await _authenticatedUserService
+                .GetAuthenticatedUserAsync(cancellationToken);
+
+            return _userMapper.Map(user);
+        }
+    }
+}
