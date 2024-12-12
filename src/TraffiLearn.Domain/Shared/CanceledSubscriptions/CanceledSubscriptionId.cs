efdcd94ff49d63bdc48cea3bbd94ab014d31b5bf
savchenkoaddev <p.savchenkoad@gmail.com>
2024-12-12 12:35:36 +0200

    Implement the repository

--- src/TraffiLearn.Domain/Shared/CanceledSubscriptions/CanceledSubscriptionId.cs
+++ src/TraffiLearn.Domain/Shared/CanceledSubscriptions/CanceledSubscriptionId.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TraffiLearn.Domain.Shared.CanceledSubscriptions
+﻿namespace TraffiLearn.Domain.Shared.CanceledSubscriptions
 {
     public readonly record struct CanceledSubscriptionId(Guid Value);
 }
---
+++ src/TraffiLearn.Domain/Shared/CanceledSubscriptions/ICanceledSubscriptionRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace TraffiLearn.Domain.Shared.CanceledSubscriptions
+{
+    public interface ICanceledSubscriptionRepository
+    {
+        Task<IEnumerable<CanceledSubscription>> GetAllAsync(
+            CancellationToken cancellationToken = default);
+
+        Task InsertAsync(
+            CanceledSubscription canceledSubscription,
+            CancellationToken cancellationToken = default);
+    }
+}
