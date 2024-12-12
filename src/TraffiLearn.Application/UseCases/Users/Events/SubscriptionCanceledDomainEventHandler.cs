9f0f02c84771f2e0c9edf17fd6b0533129f3e9db
savchenkoaddev <p.savchenkoad@gmail.com>
2024-12-12 15:51:27 +0200

    Implement the event handler

---
+++ src/TraffiLearn.Application/UseCases/Users/Events/SubscriptionCanceledDomainEventHandler.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TraffiLearn.Application.Abstractions.Data;
+using TraffiLearn.Domain.Shared.CanceledSubscriptions;
+using TraffiLearn.Domain.Users.DomainEvents;
+using TraffiLearn.SharedKernel.Shared;
+
+namespace TraffiLearn.Application.UseCases.Users.Events
+{
+    internal sealed class SubscriptionCanceledDomainEventHandler
+        : INotificationHandler<SubscriptionCanceledDomainEvent>
+    {
+        private readonly Mapper<SubscriptionCanceledDomainEvent, Result<CanceledSubscription>> _mapper;
+        private readonly ICanceledSubscriptionRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<SubscriptionCanceledDomainEventHandler> _logger;
+
+        public SubscriptionCanceledDomainEventHandler(
+            IServiceProvider serviceProvider,
+            ILogger<SubscriptionCanceledDomainEventHandler> logger)
+        {
+            var sp = serviceProvider.CreateScope().ServiceProvider;
+
+            _repository = sp.GetRequiredService<ICanceledSubscriptionRepository>();
+            _unitOfWork = sp.GetRequiredService<IUnitOfWork>();
+
+            _mapper = sp.GetRequiredService<Mapper<
+                SubscriptionCanceledDomainEvent, Result<CanceledSubscription>>>();
+
+            _logger = logger;
+        }
+
+        public async Task Handle(
+            SubscriptionCanceledDomainEvent notification,
+            CancellationToken cancellationToken)
+        {
+            var canceledSubscriptionResult = _mapper.Map(notification);
+
+            if (canceledSubscriptionResult.IsFailure)
+            {
+                throw new InvalidOperationException(
+                    "Failed to map SubscriptionCanceledDomainEvent from the storage to the CanceledSubscription entity.");
+            }
+
+            var canceledSubscription = canceledSubscriptionResult.Value;
+
+            await _repository.InsertAsync(
+                canceledSubscription,
+                cancellationToken);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            _logger.LogInformation("Succesfully inserted a CanceledSubscription Entity.");
+        }
+    }
+}
---
+++ src/TraffiLearn.Application/UseCases/Users/Mappers/SubscriptionCanceledDomainEventToEntityMapper.cs
@@ -0,0 +1,45 @@
+﻿using TraffiLearn.Application.Abstractions.Data;
+using TraffiLearn.Domain.Shared.CanceledSubscriptions;
+using TraffiLearn.Domain.SubscriptionPlans;
+using TraffiLearn.Domain.Users;
+using TraffiLearn.Domain.Users.CancelationReasons;
+using TraffiLearn.Domain.Users.DomainEvents;
+using TraffiLearn.SharedKernel.Shared;
+
+namespace TraffiLearn.Application.UseCases.Users.Mappers
+{
+    internal sealed class SubscriptionCanceledDomainEventToEntityMapper
+        : Mapper<SubscriptionCanceledDomainEvent, Result<CanceledSubscription>>
+    {
+        public override Result<CanceledSubscription> Map(
+            SubscriptionCanceledDomainEvent source)
+        {
+            var cancelationReasonResult = CancelationReason.Create(source.Reason);
+
+            if (cancelationReasonResult.IsFailure)
+            {
+                return Result.Failure<CanceledSubscription>(
+                    cancelationReasonResult.Error);
+            }
+
+            var canceledSubscriptionId = new CanceledSubscriptionId(Guid.NewGuid());
+            var subscriptionPlanId = new SubscriptionPlanId(source.SubscriptionId);
+            var userId = new UserId(source.UserId);
+
+            var canceledSubscriptionResult = CanceledSubscription.Create(
+                canceledSubscriptionId: canceledSubscriptionId,
+                userId: userId,
+                subscriptionPlanId: subscriptionPlanId,
+                cancelationReason: cancelationReasonResult.Value,
+                canceledAt: source.CanceledAt);
+
+            if (canceledSubscriptionResult.IsFailure)
+            {
+                return Result.Failure<CanceledSubscription>(
+                    canceledSubscriptionResult.Error);
+            }
+
+            return canceledSubscriptionResult.Value;
+        }
+    }
+}
