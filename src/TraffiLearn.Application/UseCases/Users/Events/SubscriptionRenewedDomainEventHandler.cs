99d4307dd3528bebbcfd691645f6f58c6f90e916
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-12-18 17:03:48 +0100

    Implement SubscriptionRenewedDomainEventHandler

---
+++ src/TraffiLearn.Application/UseCases/Users/Events/SubscriptionRenewedDomainEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TraffiLearn.Application.Abstractions.Emails;
+using TraffiLearn.Domain.Users;
+using TraffiLearn.Domain.Users.DomainEvents;
+
+namespace TraffiLearn.Application.UseCases.Users.Events
+{
+    internal sealed class SubscriptionRenewedDomainEventHandler
+        : INotificationHandler<SubscriptionRenewedDomainEvent>
+    {
+        private readonly IEmailService _emailService;
+        private readonly IUserRepository _userRepository;
+
+        public SubscriptionRenewedDomainEventHandler(IServiceProvider serviceProvider)
+        {
+            var sp = serviceProvider.CreateScope().ServiceProvider;
+
+            _emailService = sp.GetRequiredService<IEmailService>();
+            _userRepository = sp.GetRequiredService<IUserRepository>();
+        }
+
+        public async Task Handle(
+            SubscriptionRenewedDomainEvent notification,
+            CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(
+                new UserId(notification.UserId), cancellationToken);
+
+            if (user is null)
+            {
+                throw new InvalidOperationException("User is not found.");
+            }
+
+            var userEmail = user.Email.Value;
+
+            await _emailService.PublishPlanRenewedEmailAsync(
+                userEmail,
+                notification.PlanExpiresOn);
+        }
+    }
+}
