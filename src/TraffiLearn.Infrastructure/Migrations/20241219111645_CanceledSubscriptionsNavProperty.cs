12d8bcc79d57cd57d6359cdf59b5511ff1d6cba9
savchenkoaddev <p.savchenkoad@gmail.com>
2024-12-19 13:18:32 +0200

    Add navigation property to the entity

---
+++ src/TraffiLearn.Infrastructure/Migrations/20241219111645_CanceledSubscriptionsNavProperty.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TraffiLearn.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CanceledSubscriptionsNavProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CanceledSubscriptions_SubscriptionPlanId",
+                table: "CanceledSubscriptions",
+                column: "SubscriptionPlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CanceledSubscriptions_SubscriptionPlans_SubscriptionPlanId",
+                table: "CanceledSubscriptions",
+                column: "SubscriptionPlanId",
+                principalTable: "SubscriptionPlans",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CanceledSubscriptions_SubscriptionPlans_SubscriptionPlanId",
+                table: "CanceledSubscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CanceledSubscriptions_SubscriptionPlanId",
+                table: "CanceledSubscriptions");
+        }
+    }
+}
