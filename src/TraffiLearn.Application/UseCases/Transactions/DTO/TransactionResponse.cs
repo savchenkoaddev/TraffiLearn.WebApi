89af19b8dbee9481dffd0b3a3107b6be515cf3ab
savchenkoaddev <p.savchenkoad@gmail.com>
2024-12-19 11:11:31 +0200

    Implement the handler

---
+++ src/TraffiLearn.Application/UseCases/Transactions/DTO/TransactionResponse.cs
@@ -0,0 +1,11 @@
+﻿using TraffiLearn.Application.UseCases.SubscriptionPlans.DTO;
+
+namespace TraffiLearn.Application.UseCases.Transactions.DTO
+{
+    public sealed record TransactionResponse(
+        Guid Id,
+        Guid UserId,
+        SubscriptionPlanResponse SubscriptionPlan,
+        DateTime Timestamp,
+        string? Metadata);
+}
---
+++ src/TraffiLearn.Application/UseCases/Users/Queries/GetCurrentUserTransactions/GetCurrentUserTransactionsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TraffiLearn.Application.UseCases.Transactions.DTO;
+
+namespace TraffiLearn.Application.UseCases.Users.Queries.GetCurrentUserTransactions
+{
+    public sealed record GetCurrentUserTransactionsQuery 
+        : IRequest<Result<IEnumerable<TransactionResponse>>>;
+}
