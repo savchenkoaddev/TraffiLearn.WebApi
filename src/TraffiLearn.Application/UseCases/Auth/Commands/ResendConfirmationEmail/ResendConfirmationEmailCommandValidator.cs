a2f00ed66a39e0ef4d35fc3b6e731f0c2e50ced8
savchenkoaddev <p.savchenkoad@gmail.com>
2024-12-20 13:36:17 +0200

    Implement the handler

---
+++ src/TraffiLearn.Application/UseCases/Auth/Commands/ResendConfirmationEmail/ResendConfirmationEmailCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TraffiLearn.Domain.Users.Emails;
+
+namespace TraffiLearn.Application.UseCases.Auth.Commands.ResendConfirmationEmail
+{
+    internal sealed class ResendConfirmationEmailCommandValidator
+        : AbstractValidator<ResendConfirmationEmailCommand>
+    {
+        public ResendConfirmationEmailCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .Matches(@"^[^@\s]+@[^@\s]+\.[^@\s]+$").WithMessage("Email is not in the proper format.")
+                .MaximumLength(Email.MaxLength)
+                .NotEmpty();
+        }
+    }
+}
